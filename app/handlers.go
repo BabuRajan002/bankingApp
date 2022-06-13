fb5f3822d27b40e09d00920ed94dcdddb2ff296b
babu.sundararajan <babu.sundararajan@rakuten.com>
2022-06-13 15:58:55 +0530

    Updated with DB adapter

--- app/handlers.go
+++ app/handlers.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Customer struct {
-	Name    string `json: "full_name" xml:"name"`
+	Name    string `json:"full_name" xml:"name"`
 	City    string `json:"city" xml:"city"`
 	Zipcode string `json:"zip_code" xml:"zipcode"`
 }
