7bb6c10e440b75be9389b42b95f1082e33862907
babu.sundararajan <babu.sundararajan@rakuten.com>
2022-06-14 18:06:47 +0530

    Updated with refactor

--- app/customerHandlers.go
+++ app/customerHandlers.go
@@ -9,21 +9,11 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Customer struct {
-	Name    string `json:"full_name" xml:"name"`
-	City    string `json:"city" xml:"city"`
-	Zipcode string `json:"zip_code" xml:"zipcode"`
-}
-
 type CustomerHandlers struct {
 	service service.CustomerService
 }
 
 func (ch *CustomerHandlers) getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	//customers := []Customer{
-	//	{Name: "Babu", City: "Tamil Nadu", Zipcode: "621312"},
-	//	{Name: "Vinna", City: "Tamil Nadu", Zipcode: "123456"},
-	//}
 
 	customers, _ := ch.service.GetAllCustomer()
 
