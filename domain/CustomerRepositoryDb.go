5996ea8fdda87d36133d6c4bfc6dcd1c54b94fff
babu.sundararajan <babu.sundararajan@rakuten.com>
2022-06-14 17:26:58 +0530

    Updated with Error handling part2

--- domain/CustomerRepositoryDb.go
+++ domain/CustomerRepositoryDb.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"bankingApp/errs"
 	"database/sql"
 	"log"
 	"time"
@@ -37,15 +38,19 @@ func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
 
 // Add implementation for the method ById as part of this port CustomerRepository interface
 
-func (d CustomerRepositoryDb) ById(id string) (*Customer, error) {
+func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppError) {
 	customerSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers where customer_id = ?"
 
 	row := d.client.QueryRow(customerSql, id)
 	var c Customer
 	err := row.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateofBith, &c.Status)
 	if err != nil {
-		log.Println("Error while scanning customer " + err.Error())
-		return nil, err
+		if err == sql.ErrNoRows {
+			return nil, errs.NewNotFoundError("Customer not found")
+		} else {
+			log.Println("Error while scanning customer " + err.Error())
+			return nil, errs.NewUnexpectedError("Unexpected Database error")
+		}
 	}
 	return &c, nil
 
