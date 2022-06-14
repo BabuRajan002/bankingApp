338d8a0afa4f816e705fe000a1715e895d968624
babu.sundararajan <babu.sundararajan@rakuten.com>
2022-06-14 18:20:21 +0530

    Fixed GetAllAPI

--- app/customerHandlers.go
+++ app/customerHandlers.go
@@ -3,7 +3,6 @@ package app
 import (
 	"bankingApp/service"
 	"encoding/json"
-	"encoding/xml"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -15,14 +14,11 @@ type CustomerHandlers struct {
 
 func (ch *CustomerHandlers) getAllCustomers(w http.ResponseWriter, r *http.Request) {
 
-	customers, _ := ch.service.GetAllCustomer()
-
-	if r.Header.Get("Content-Type") == "application/xml" {
-		w.Header().Add("Content-Type", "application/xml")
-		xml.NewEncoder(w).Encode(customers)
+	customers, err := ch.service.GetAllCustomer()
+	if err != nil {
+		writeResponse(w, err.Code, err.AsMessage())
 	} else {
-		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(customers)
+		writeResponse(w, http.StatusOK, customers)
 	}
 
 }
